8c77776eebd9258242edd03ef8558cbdf9423ca4
Kacper Dusza <dusza-kacper@wp.pl>
2023-08-10 00:16:59 +0200

    Building error messages
    I'm currently building some error messages in case of doubling emails and usernames in database. I'll leave it as it is for now, and think about how to make it the most efficient way.

--- includes/Register.php
+++ includes/Register.php
@@ -19,12 +19,31 @@ class User extends Database
         
 }
 
+
 Class Register extends User
 {
     public function registerIn($username,$password,$email) 
     {
         $query_insert = "INSERT INTO users (username, password, email) VALUES ('$username','$password','$email')";
+        $stmt_insert = $this->connect()->query($query_insert);
 
-        $this->connect()->query($query_insert);
+        $query_email= "SELECT * FROM users WHERE username = '$email'";
+        $stmt_select_email = $this->connect()->query($query_email);
+
+        $query_user= "SELECT * FROM users WHERE username = '$username'";
+        $stmt_select_user = $this->connect()->query($query_user);
+
+        
+
+        if(mysqli_num_rows($stmt_select_email) > 1)
+        { 
+            exit("That email is already taken!");
+        }
+/*
+        if(mysqli_num_rows($stmt_select_user) > 1)
+        {
+            echo "This user already exists!";
+        }
+*/        
     }
 }
