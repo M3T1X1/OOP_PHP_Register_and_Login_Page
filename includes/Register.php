4fb822f9aff70091ef420acde74b962258a3aef4
Kacper Dusza <112811911+M3T1X1@users.noreply.github.com>
2023-08-19 23:48:06 +0200

    Update Register.php

--- includes/Register.php
+++ includes/Register.php
@@ -24,25 +24,20 @@ Class Register extends User
 {
     public function registerIn($email,$username,$password) 
     {
-        $query_insert = "INSERT INTO users (email, password, username) VALUES ('$email','$password','$username')";
-        
-
-        $query_select_username = "SELECT * FROM users WHERE username = '$username'";
-        $stmt_select_username = $this->connect()->query($query_select_username);
-
-        $query_select_email = "SELECT * FROM users WHERE email = '$email'";
-        $stmt_select_email = $this->connect()->query($query_select_email);
-
-            if(mysqli_num_rows($stmt_select_username) >= 1)
-            {
-            echo "User exists";
-            }
-
-            if(mysqli_num_rows($stmt_select_email) >= 1)
-            {
-             echo "Email exists";
-            }
        
+       
+       
+       
+       
+       
+        /*
+        else
+        {
+        $query_insert = "INSERT INTO users (email, password, username) VALUES ('$email','$password','$username')";
+        $stmt_insert = $this->connect()->query($query_insert);
+        }
+        */
+
     
 
    
